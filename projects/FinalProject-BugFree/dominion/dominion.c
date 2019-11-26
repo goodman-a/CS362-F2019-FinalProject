5f236601903e55c41487717fb63a36e2c9823b57
Cris Shumack <shumackc@flip1.engr.oregonstate.edu>
2019-11-26 11:02:19 -0800

    Added unittest01.c

--- projects/FinalProject-BugFree/dominion/dominion.c
+++ projects/FinalProject-BugFree/dominion/dominion.c
@@ -798,27 +798,29 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
             return -1;
         }
 
-        if (choice2 > treasure_map || choice2 < curse)
+        if (choice2 > gold || choice2 < copper)
         {
             return -1;
         }
 
-        if ( (getCost(state->hand[currentPlayer][choice1]) + 3) > getCost(choice2) )
+        if ( (getCost(state->hand[currentPlayer][choice1]) + 3) < getCost(choice2) )
         {
             return -1;
         }
+
+	if (state->supplyCount[choice2] <= 0)
+	{
+		return -1;
+	}
 
         gainCard(choice2, state, 2, currentPlayer);
-
-        //discard card from hand
-        discardCard(handPos, currentPlayer, state, 0);
 
         //discard trashed card
         for (i = 0; i < state->handCount[currentPlayer]; i++)
         {
             if (state->hand[currentPlayer][i] == j)
             {
-                discardCard(i, currentPlayer, state, 0);
+                discardCard(i, currentPlayer, state, 1);
                 break;
             }
         }
